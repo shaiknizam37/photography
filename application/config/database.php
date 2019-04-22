66d9e0a915266f74da5a2eef1e8ae6551799f287
shaiknizam37 <shaiknizam37@gmail.com>
2019-04-22 17:30:19 +0530

    changes

--- application/config/database.php
+++ application/config/database.php
@@ -75,11 +75,11 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'ec2-18-188-72-202.us-east-2.compute.amazonaws.com',
+	'hostname' => 'localhost',
 	'username' => 'shaik',
 	'password' => 'Shaik@123',
 	'database' => 'photography_club',
-	'dbdriver' => 'mysqli',
+	'dbdriver' => 'mysql',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
