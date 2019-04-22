202783a90393de94d2fe76533a7e35c63009d222
shaiknizam37 <shaiknizam37@gmail.com>
2019-04-22 18:01:26 +0530

    changes

---
+++ sample.php
@@ -0,0 +1,8 @@
+<?php
+$con = mysqli_connect("localhost","shaik","Shaik@123","photography_club");
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
