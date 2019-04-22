b7f55246800d421a597e7adab66e1f5ed874ff93
shaiknizam37 <shaiknizam37@gmail.com>
2019-04-22 18:09:50 +0530

    changes

--- application/config/database.php
+++ application/config/database.php
@@ -74,7 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> '',
+	'dsn'	=> 'mysql:host=localhost;port=3360;dbname=photography_club',
 	'hostname' => 'localhost',
 	'username' => 'shaik',
 	'password' => 'Shaik@123',
