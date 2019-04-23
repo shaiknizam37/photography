e140066d1e557ea65f8fe385e2341d2da2837f63
shaiknizam37 <shaiknizam37@gmail.com>
2019-04-23 16:58:11 +0530

    changes

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'cctcabs.com.mysql',
-	'username' => 'cctcabs_com',
-	'password' => 'cctcabs123',
-	'database' => 'cctcabs_com',
+	'hostname' => 'localhost',
+	'username' => '',
+	'password' => '',
+	'database' => '',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
