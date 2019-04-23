4e0c7fd6dd63d5ff0cf876707eb25b42ef83ab03
shaiknizam37 <shaiknizam37@gmail.com>
2019-04-23 14:28:34 +0000

    shainges

--- application/config/database.php
+++ application/config/database.php
@@ -81,7 +81,7 @@ $db['default'] = array(
 	'database' => 'photography_club',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
-	'pconnect' => TRUE,
+	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
