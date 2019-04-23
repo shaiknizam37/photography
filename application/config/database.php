4f017caa70a47ea4a89440f43416f30ce14206ef
shaiknizam37 <shaiknizam37@gmail.com>
2019-04-23 18:01:13 +0530

    changes

--- application/config/database.php
+++ application/config/database.php
@@ -74,6 +74,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
+	'dsn'=>''
 	'hostname' => 'photographyclub.cggckxxct9vq.us-east-2.rds.amazonaws.com',
 	'username' => 'shaik',
 	'password' => 'Shaik123',
